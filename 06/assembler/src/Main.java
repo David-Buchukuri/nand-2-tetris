b058b84a603734d3289322bab82d458f86c16b1b
David-Buchukuri <datobuchukuri20@gmail.com>
2024-04-24 02:31:04 +0400

    assembler: project setup

---
+++ 06/assembler/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if(args.length == 0){
+            throw new Exception("path must be passed as an argument");
+        }
+    }
+}
